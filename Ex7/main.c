9276ab3b82e8e947644716d5562b891775e63fba
student <student@Sanntid.(none)>
2014-10-10 13:50:52 +0200

    done with assisgnmentA

--- Ex7/main.c
+++ Ex7/main.c
@@ -4,189 +4,107 @@
 #include <sys/mman.h>
 #include <native/task.h>
 #include <native/timer.h>
+#include <native/sem.h>
 #include <rtdk.h>
 
-#define TEST_MODE false
-
-#if TEST_MODE
-/****************************************** TEST PROG******************************************/
-RT_TASK demo_task;
-
-void demo(void *arg){
-    RT_TASK *curtask;
-    RT_TASK_INFO curtaskinfo;
-
-    /* hello world */
-    rt_printf("Hello World!\n");
-
-    /* inquire current task */
-    curtask=rt_task_self();
-
-    rt_task_inquire(curtask,&curtaskinfo);
-
-    /* print task name */
-    rt_printf("Task name : %s \n", curtaskinfo.name);
-}
-
-int main(int argc, char* argv[]){
-    char  str[10] ;
-
-    /* Perform auto-init of rt_print buffers if the task doesn't do so */
-    rt_print_auto_init(1);
-
-    /* Lock memory : avoid memory swapping for this program */
-    mlockall(MCL_CURRENT|MCL_FUTURE);
-
-    rt_printf("start task\n");
-    sprintf(str,"hello");
- 
-
-    /*
-    * Arguments: &task,
-    *            name,
-    *            stack size (0=default),
-    *            priority,
-    *            mode (FPU, start suspended, ...)
-    */
-    rt_task_create(&demo_task, str, 0, 50, 0);
-
- 
-    /*
-    * Arguments: &task,
-    *            task function,
-    *            function argument
-    */
-    rt_task_start(&demo_task, &demo, 0);
-}
-
-#else
 /*************************************** USER PROG *************************************/
 #include <unistd.h> //usleep
 #include <stdlib.h> //malloc, free
 #include <pthread.h>
 #include <sched.h>
-#include "io.h"
 
-#define N_RT_THREADS    3
-#define N_PTHREADS      10
-#define RT_THREADS_PRI  50
-#define SLEEP_PERIOD    9000
+#define CPU_ID          0
+
+#define N_RT_THREADS    2
+
+#define RT_TASK0_PRI    2
+#define RT_TASK1_PRI    4
+#define RT_MAIN_PRI     10
+
+#define SLEEP_DELAY    100000000
 #define SUCCESS         0
 
 static void *RT_function(void *argpointer);
-static void *pthread_function(void *argpointer);
-int set_cpu(int cpu_number);
+RT_SEM BARRIER;
 
 int main(void){
-    
+
     /* Perform auto-init of rt_print buffers if the task doesn't do so */
     rt_print_auto_init(1);
 
     /* Lock memory : avoid memory swapping for this program */
     mlockall(MCL_CURRENT|MCL_FUTURE);
+    
+    /* Make main a Real-Time thread */
+    if(rt_task_shadow(NULL, NULL, RT_MAIN_PRI, T_CPU(CPU_ID) ) != SUCCESS){
+        printf("RT Thread main failed to be created!\n");
+        exit(1);
+    }else{
+        /* print success */
+        rt_printf("RT Thread main initiated successfully \n");
+    }
 
-    /* init hardware */
-    int ret=io_init();
-    if(ret < 1){
-        printf("io init failed");
-        return 1;
-    } 
+    /* Create Barrier */
+    if(rt_sem_create(&BARRIER, "Barrier", 0, S_PRIO) != SUCCESS){
+        rt_printf("BARRIER semaphore failed to be created \n");
+        exit(1);
+    }
 
     /* Initialize Real-Time threads */
     int* chan;
 	int i;
     RT_TASK rt_threads[N_RT_THREADS];
+    int RT_THREADS_PRI[]= {RT_TASK0_PRI, RT_TASK1_PRI};
+
     for(i=0; i<N_RT_THREADS; i++){
         /* Give the new thread the channel to respond to by argument */
         chan = malloc(sizeof(int));
-        *chan = i+1;
+        *chan = i;
         
         /* Create new thread */
-        if(rt_task_create(&rt_threads[i], NULL, 0, RT_THREADS_PRI, 0) != SUCCESS){
-            printf("RT Thread %i failed to be created!\n", i);
-            exit(1);
-        }
-
-        /* Task is to execute periodicly */
-        if(rt_task_set_periodic(&rt_threads[i], TM_NOW, SLEEP_PERIOD) != SUCCESS){ 
-            printf("RT Thread %i failed to set period!\n", i);
+        if(rt_task_create(&rt_threads[i], NULL, 0, RT_THREADS_PRI[i], T_CPU(CPU_ID)|T_JOINABLE) != SUCCESS){
+            rt_printf("RT Thread %i failed to be created!\n", i);
             exit(1);
         }
 
         /* Execute task */
         if(rt_task_start(&rt_threads[i], RT_function, chan) != SUCCESS){
-            printf("RT Thread %i failed to start!\n", (void*) i);
+            rt_printf("RT Thread %i failed to start!\n", (void*) i);
             exit(1);
         }
 
         /* print success */
-        printf("RT Thread  %i initiated successfully \n", i);
+        rt_printf("RT Thread  %i initiated successfully \n", i);
+    }
+    
+    /* Wait untill all threads have been blocked, restart them */
+    rt_task_sleep(SLEEP_DELAY);
+    if(rt_sem_broadcast(&BARRIER)!= SUCCESS){
+        rt_printf("main thread failed to broadcast on BARRIER semaphore \n");
+        exit(1);
+    }
+    rt_task_sleep(SLEEP_DELAY);
+
+    /* Collect all threads */
+    for(i=0; i<N_RT_THREADS; i++){
+        rt_task_join(&rt_threads[i]);
     }
 
-    /* Initialize disturbance threads */
-    pthread_t pthreads[N_PTHREADS];
-
-    for(i=0; i<N_PTHREADS; i++){
-
-        /* Create new thread */
-        if(pthread_create(&pthreads[i], NULL, pthread_function, NULL) != SUCCESS){
-            printf("Disturbance thread %i failed to be created \n", i);
-            exit(1);
-        }
-
-        /* print success */
-        printf("Pthread %i initiated successfully \n", i);
-    }
-
-    /* Collect threads before exiting */
-	int j;
-    for(j = 0; j < N_PTHREADS; j++){
-        pthread_join(pthreads[j], NULL);
-    }   
+    /* Clean up */
+    rt_sem_delete(&BARRIER);
 
     return 0;
 }
 
 /* Polls the signal line given by argument and reacts on the corresponding line */
 static void *RT_function(void *argpointer){
-    //set cpu
 
     int chan = *((int *) argpointer);
     free(argpointer);
 
-    while(1){
-        rt_task_wait_period(NULL);
-
-        if(io_read(chan) == 0){
-            io_write(chan, 0);
-            usleep(5);
-            io_write(chan, 1);
-        }
-    }   
-}
-
-static void *pthread_function(void *argpointer){
-    set_cpu(1);
-    int retardation_lvl;
-
-    while(1){
-        retardation_lvl++;
-        
-        if(retardation_lvl>9000){
-            retardation_lvl = 0;
-        }
+    if(rt_sem_p(&BARRIER, TM_INFINITE) == SUCCESS){
+        rt_printf("Task %i freed\n", chan);
+    }else{
+        rt_printf("Task %i failed to get semaphore \n", chan); 
     }
 }
-
-int set_cpu(int cpu_number)
-{
-	/* setting cpu set to the selected cpu */
-	cpu_set_t cpu;
-	CPU_ZERO(&cpu);
-	CPU_SET(cpu_number, &cpu);
-
-	/* set cpu set to current thread and return */
-	return pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t),&cpu);
-}
-
-#endif
